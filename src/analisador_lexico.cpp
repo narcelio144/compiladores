c934c8660e87cd0eec64aafb5fdd6e14d3992475
narcelio144 <narcelio@outlook.com>
2017-08-07 17:31:58 -0300

    Adding typedefs and initial functions.

--- src/analisador_lexico.cpp
+++ src/analisador_lexico.cpp
@@ -1,3 +1,25 @@
 void mope(){
+	typedef enum {
+		// palavras reservadas
+		ARRAY, BOOLEAN, BREAK, CHAR, CONTINUE, DO, ELSE, FALSE, FUNCTION, IF, INTEGER, OF, STRING, STRUCT, TRUE, TYPE, VAR, WHILE,
+		// simbolos
+		COLON, SEMI_COLON, COMMA, EQUALS, LEFT_SQUARE, RIGHT_SQUARE, LEFT_BRACES, RIGHT_BRACES, LEFT_PARENTHESIS, RIGHT_PARENTHESIS, AND, OR, LESS_THAN, GREATER_THAN, LESS_OR_EQUAL, GREATER_OR_EQUAL, NOT_EQUAL, EQUAL_EQUAL, PLUS, PLUS_PLUS, MINUS, MINUS_MINUS, TIMES, DIVIDE, DOT, NOT,
+		// tokens regulares
+		CHARACTER, NUMERAL, STRINGVAL, ID,
+		// token deconhecido
+		UNKNOWN 
+	} t_token;
+
+	t_token searchKeyWord( char *name );
+	int searchName( char *name );
+	typedef struct{
+		byte type; // 0-char, 1-int, 2- string union
+		{
+            char
+            int
+            char *
+		} _; 
+	} t_const;
+
 }
 
